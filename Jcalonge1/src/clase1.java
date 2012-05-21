286d08e935ed570237ef745645bfeada186a6116
jcalonge <jcalonge@106-29.info2>
2012-05-21 13:39:19 +0200

    metodos

--- Jcalonge1/src/clase1.java
+++ Jcalonge1/src/clase1.java
@@ -4,6 +4,10 @@ public class clase1 {
 	/**
 	 * @param args
 	 */
+	public static void metodo1_clase1()
+	{
+		
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Soy la clase 1");
--- Jcalonge1/src/clase2.java
+++ Jcalonge1/src/clase2.java
@@ -4,6 +4,10 @@ public class clase2 {
 	/**
 	 * @param args
 	 */
+	public static void metodo1_clase2()
+	{
+		
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Soy la clase 2");
