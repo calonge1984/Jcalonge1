ba41e5b3b295bbd10aa438b2202e27408c25a979
jcalonge <jcalonge@106-29.info2>
2012-05-21 13:41:36 +0200

    rama_master

--- Jcalonge1/src/clase2.java
+++ Jcalonge1/src/clase2.java
@@ -8,6 +8,10 @@ public class clase2 {
 	{
 		
 	}
+	public static void metodo_rama_mater_clase2()
+	{
+		
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Soy la clase 2");
